3907d42491c59398246e430d37650b05fe8671e7
Fue Lee <fueclee.12@gmail.com>
2023-03-31 18:31:49 -0500

    initial HTML and importing information from Redux store completed

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
+
 function Review() {
     console.log('Review running');
 
+    const feeling = useSelector(store => store.feeling)
+    const understanding = useSelector(store => store.understanding)
+    const supported = useSelector(store => store.supported)
+    const comments = useSelector(store => store.comments)
+
     return(
         <>
-            <h1>Review</h1>
+            <h2>Review Your Feedback</h2>
+            <p>Feelings: {feeling}</p>
+            <p>Understanding: {understanding}</p>
+            <p>Support: {supported}</p>
+            <p>Comments: {comments}</p>
             <button>Submit</button>
         </>
     )
