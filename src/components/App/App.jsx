e5f6aa609e63dde68a71240a64801281f6653c74
Fue Lee <fueclee.12@gmail.com>
2023-03-31 15:11:34 -0500

    created components based on estimated need and page breakdowns

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,36 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+import { useSelector } from "react-redux";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function App() {
 
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
     </div>
+    <Route exact path='/'>
+      <FeelingsRating />
+    </Route>
+    <Route exact path='/understanding'>
+      <UnderstandingRating />
+    </Route>
+    <Route exact path='/supported'>
+      <SupportedRating />
+    </Route>
+    <Route exact path='/comments'>
+      <Comments />
+    </Route>
+    <Route exact path='/review'>
+      <Review />
+    </Route>
+    </Router>
   );
 }
 
